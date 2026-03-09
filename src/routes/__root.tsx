21158bcdcc94c25f878838b2ed6263fb49375c2d
StickyCoolDev <hujifa.info@gmail.com>
2026-03-09 14:42:21 +0000

    fix type error

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,10 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      {process.env.NODE_ENV === 'development' && <TanStackRouterDevtools />}
     </>
   ),
 })
